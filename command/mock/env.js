5c2a7a62fbe99e191874e60e1e991cc0bc444b4a
rich66 <2312785838@qq.com>
2023-07-18 10:04:38 +0000

    !7 fix: mock add proxy * feat: mock support proxy

--- command/mock/env.js
+++ command/mock/env.js
@@ -3,10 +3,17 @@ module.exports = {
     action: './mock/action',
     restful: './mock/restful',
   },
-  reqestLimit: '50mb',
+  requestLimit: '50mb',
   notFoundResponse: {
     RetCode: 400,
     Message: 'NotFound',
     Data: null,
+  },
+  cors: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Allow-Methods': 'PUT, POST, GET, DELETE, OPTIONS',
+    'Content-Type': 'application/json;charset=utf-8',
   },
 };
